eef3ecdd17236cb640113411977705f3e1af2765
pohky <otoriosu@gmail.com>
2024-05-15 15:02:28 +0200

    more cleanup
    some more changes mentioned by @Haselnussbomber

--- FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyPartyInvite.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyPartyInvite.cs
@@ -8,6 +8,6 @@ public unsafe partial struct InfoProxyPartyInvite {
     [FieldOffset(0x000)] public InfoProxyInvitedInterface InfoProxyInvitedInterface;
     [FieldOffset(0x03C)] public uint InviteTime;
     [FieldOffset(0x042)] public ushort InviterWorldId;
-    [FieldOffset(0x048)] public Utf8String IviterName;
-    [FieldOffset(0x0B0)] public Utf8String IviterNameWithHomeworld;
+    [FieldOffset(0x048)] public Utf8String InviterName;
+    [FieldOffset(0x0B0)] public Utf8String InviterNameWithHomeworld;
 }
